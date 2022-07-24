f3a802f6077a94a2b63bbb78e35ee70337c7c6a8
kanoute-coumba <kanoute.coumba@gmail.com>
2022-07-24 10:06:49 +0000

    adding ES6

---
+++ es6.js
@@ -0,0 +1,17 @@
+// function looper(){
+    // var est de scoope fonction
+//     for(var i=0; i<5; i++){
+//         console.log(i)
+
+//     }
+//     console.log(i)
+// }
+// looper()
+
+function calcul(){
+    for (let i=0; i<6; i++) {
+        console.log(i)
+    }
+    console.log(i)
+}
+calcul()
